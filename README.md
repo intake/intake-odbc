93ce7ee9ebe7cb5fbd4ee26594ecd261f04cccee
Martin Durant <martin.durant@utoronto.ca>
2018-01-26 10:25:31 -0500

    Begin transition to turbodbc

--- README.md
+++ README.md
@@ -14,10 +14,19 @@ TBD
     # TDB: Fix these install instructions
     conda install -c intake intake
     conda install -n root conda-build
-    conda install pandas sqlalchemy 
+    conda install pandas turbodbc
     ```
 
 1. Development installation:
     ```
     python setup.py develop --no-deps
     ```
+
+1. Create a DB to connect to. You can do this by bootstrapping a MySQL
+    image in docker, e.g.,
+
+    ```
+    docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Strong(!)Password' \
+        -p 1433:1433 -d microsoft/mssql-server-linux:2017-CU3
+    ```
+    and follow the [configuration instructions](http://turbodbc.readthedocs.io/en/latest/pages/databases/mysql.html).
--- intake_odbc/intake_odbc.py
+++ intake_odbc/intake_odbc.py
@@ -1,4 +1,5 @@
 from intake.source import base
+from turbodbc import connect
 
 
 class Plugin(base.Plugin):
@@ -18,12 +19,24 @@ class Plugin(base.Plugin):
         """
         base_kwargs, source_kwargs = self.separate_base_kwargs(kwargs)
         return ODBCSource(uri=uri,
-                              sql_expr=sql_expr,
-                              odbc_kwargs=source_kwargs,
-                              metadata=base_kwargs['metadata'])
+                          sql_expr=sql_expr,
+                          odbc_kwargs=source_kwargs,
+                          metadata=base_kwargs['metadata'])
 
 
 class ODBCSource(base.DataSource):
+    """
+    Options [docs](http://turbodbc.readthedocs.io/en/latest/pages/advanced_usage.html#advanced-usage)
+
+    Connection string [docs](http://turbodbc.readthedocs.io/en/latest/pages/getting_started.html#establish-a-connection-with-your-database)
+
+    Parameters
+    ----------
+    head_rows: int (10)
+        Number of rows that are read from the start of the data to infer data
+        types upon discovery
+    """
+
     def __init__(self, uri, sql_expr, odbc_kwargs, metadata):
         self._init_args = {
             'uri': uri,
@@ -34,27 +47,30 @@ class ODBCSource(base.DataSource):
 
         self._uri = uri
         self._sql_expr = sql_expr
+        self._head_rows = odbc_kwargs.pop('head_rows', 10)
         self._odbc_kwargs = odbc_kwargs
         self._dataframe = None
+        self._connection = None
+        self._cursor = None
 
         super(ODBCSource, self).__init__(container='dataframe',
                                          metadata=metadata)
 
     def _get_schema(self):
         if self._dataframe is None:
+            self._connection = connect(**self._odbc_kwargs)
+            cursor = self._connection.cursor()
+            self._cursor = cursor
             # This approach is not optimal; LIMIT is know to confuse the query
             # planner sometimes. If there is a faster approach to gleaning
             # dtypes from arbitrary SQL queries, we should use it instead.
-            first_row = ODBC_FIXME(
-                self._uri,
-                dataframe=True,
-                query=('({}) limit 1').format(self._sql_expr),
-                **self._odbc_kwargs
-            )._to_dataframe()
-            dtype = list(zip(first_row.dtypes.index, first_row.dtypes))
-            shape = (None, len(first_row.dtypes.index))
+            cursor.execute("SELECT * FROM ({}) LIMIT {}".format(
+                self._sql_expr, self._head_rows))
+            head = cursor.fetchallarrow().to_pandas()
+            dtype = head[:0]
+            shape = (None, head.shape[1])
         else:
-            dtype = list(zip(self._dataframe.dtypes.index, self._dataframe.dtypes))
+            dtype = self._dataframe[:0]
             shape = self._dataframe.shape
         return base.Schema(datashape=None,
                            dtype=dtype,
@@ -64,13 +80,8 @@ class ODBCSource(base.DataSource):
 
     def _get_partition(self, _):
         if self._dataframe is None:
-            self._dataframe = ODBC_FIXME(
-                self._uri,
-                dataframe=True,
-                query=self._sql_expr,
-                **self._odbc_kwargs
-            )._to_dataframe()
-            # The schema should be corrected once the data is read.
+            self._cursor.execute(self._sql_expr)
+            self._dataframe = self._cursor.fetchallarrow().to_pandas()
             self._schema = None
         return self._dataframe
 
